8a297e0256a2986cc60be200ff91ee510018be52
Irunnn16 <khoirunnugraha145@gmail.com>
2024-05-17 19:48:04 +0800

    ini punyaku

--- pages/about.php
+++ pages/about.php
@@ -9,6 +9,6 @@
 <body>
     <h1>About us</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus est assumenda ipsum repellat dolorum nihil obcaecati consectetur velit, rerum, nam ad autem? Possimus laborum labore expedita asperiores. Quasi, debitis vel.</p>
-    <img src ="assets/resto-about.jpeg" href="assets/resto">
+    <img src ="assets/resto-about.jpeg" href="assets/resto-about.jpeg">
 </body>
 </html>
