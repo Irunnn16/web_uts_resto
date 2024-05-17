b7bc9c19d824c8a709c33a427db457913aada05a
Irunnn16 <khoirunnugraha145@gmail.com>
2024-05-17 19:42:15 +0800

    ini

--- pages/about.php
+++ pages/about.php
@@ -7,9 +7,8 @@
     <title>Document</title>
 </head>
 <body>
-
     <h1>About us</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus est assumenda ipsum repellat dolorum nihil obcaecati consectetur velit, rerum, nam ad autem? Possimus laborum labore expedita asperiores. Quasi, debitis vel.</p>
-    
+    <img src
 </body>
 </html>
