1e7665fb63a9bc175cce44e9cd4d21fc4dd55076
Irunnn16 <khoirunnugraha145@gmail.com>
2024-05-17 15:34:01 +0800

    ini coba liat

--- pages/about.php
+++ pages/about.php
@@ -1,1 +1,15 @@
 <!-- Irun Bikin Halaman About nya -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1>About us</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus est assumenda ipsum repellat dolorum nihil obcaecati consectetur velit, rerum, nam ad autem? Possimus laborum labore expedita asperiores. Quasi, debitis vel.</p>
+    
+</body>
+</html>
